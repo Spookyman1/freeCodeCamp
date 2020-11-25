8eb2f540fdd7634a33ba4885b98ee306040ea7d3
HenBitonBIU <45950041+HenBitonBIU@users.noreply.github.com>
2020-11-25 15:14:36 +0200

    fixed label open tag and span close tag bug

--- redirections.js
+++ redirections.js
@@ -1,8 +1,9 @@
 function backToForm() {
     console.log("lol")
-    window.location.replace("form.html");
+    window.location.replace("~/form.html");
 }
 function success() {
     console.log("redi")
-    window.location.href = "submitted.html";
+    //window.location.replace = "submitted.html";
+    return false;
 }
